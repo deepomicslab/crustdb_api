96af5dde78c7c89e9e9758ad237137ae6709f29f
lst <71402513+Yecats77@users.noreply.github.com>
2024-03-07 22:16:15 +0800

    add settings_local_example
    donot know why settings_local.py cannot be uploaded
    Just upload settings_local_example.py and then modify it in the server later

--- Phage_api/settings_local_example.py
+++ Phage_api/settings_local_example.py
@@ -7,21 +7,16 @@ DATABASES = {
     'PORT': '5432',
 }
 
-# DATABASES = {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'NAME': 'crustdb',
-#     'USER': 'postgres',
-#     'PASSWORD': 'wAFQ63y!AEsH',
-#     'HOST': 'localhost',
-#     'PORT': '5432',
-# }
-
 USERTASKPATH = 'workspace/user_task'
 PHAGEFASTA = '/home/platform/phage_db/phage_data/data/phage_sequence/phage_fasta/'
 PROTEINSEQUENCE = '/home/platform/phage_db/phage_data/data/phage_sequence/proteins/'
 TEMPPATH = '/home/platform/phage_db/phage_data/data/tmp/'
 CLUSTERTREEPATH = '/home/platform/phage_db/phage_data/data/phage_sequence/group_tree/'
-METADATA = '/home/platform/phage_db/phage_api/workspace/csv_data/'
+# METADATA = '/home/platform/phage_db/phage_api/workspace/csv_data/'
+METADATA = '/mnt/e/Documents/Project/03-ST_Conformation_website/crustdb_platform/data/files/'
+PHAGEFASTA = '/mnt/e/Documents/Project/03-ST_Conformation_website/crustdb_platform/data/fasta/'
+PHAGEGBK = '/mnt/e/Documents/Project/03-ST_Conformation_website/crustdb_platform/data/'
+PHAGEGFF = '/mnt/e/Documents/Project/03-ST_Conformation_website/crustdb_platform/data/'
 # Analysis script path
 ANALYSIS = '/home/platform/phage_db/phage_api/workspace/analysis_script/'
 TASKLOG = '/home/platform/phage_db/phage_api/workspace/task_log/'
