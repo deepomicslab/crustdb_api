b7278ee494ca06d15f5ac881fd3beabc4d8a52bc
Yecats77 <liusitong327@gmail.com>
2024-04-04 21:41:05 +0800

    [2024/4/4] CyGraph Liost page filtering func [corrected]

--- crustdb_main/views.py
+++ crustdb_main/views.py
@@ -39,17 +39,46 @@ class crustdbMainViewSet(APIView):
         # print('========================== get')
         # print('========================== request', request.query_params.dict())
         querydict = request.query_params.dict()
+
+        if 'filter' in querydict:
+            filter = json.loads(querydict['filter'])
+            # print('================== filter', filter)
+            # if filter['st_platform'] or filter['species'] or filter['disease_stage'] or filter['developmental_stage'] or filter['sex'] or filter['cell_type']:
+                # print(filter['species'], filter['species'] == None)
+            q_expression = Q()
+            if filter['st_platform']:
+                q_expression &= Q(st_platform__in = filter['st_platform'])
+            if filter['species']:
+                q_expression &= Q(species__in = filter['species'])
+            if filter['disease_stage']:
+                q_expression &= Q(disease_stage__in = filter['disease_stage'])
+            if filter['developmental_stage']:
+                q_expression &= Q(developmental_stage__in = filter['developmental_stage'])
+            if filter['sex']:
+                q_expression &= Q(sex__in = filter['sex'])
+            if filter['cell_type']:
+                q_expression &= Q(cell_type__in = filter['cell_type'])
+                # print('============ q_expression', q_expression)
+            # print('=======length', len(self.queryset.all()))
+            self.queryset = crustdb_main.objects.filter(q_expression)
+            # print('=======length', len(self.queryset.all()))
+
+
         order = ''
         columnKey = ''
         if 'order' in querydict and 'columnKey' in querydict and querydict['columnKey'] != '':
             order = querydict['order']
             columnKey = querydict['columnKey']
             if order == 'false':
-                self.queryset = crustdb_main.objects.order_by('id')
+                self.queryset = self.queryset.order_by('id')
+                # self.queryset = crustdb_main.objects.order_by('id')
             elif order == 'ascend':
-                self.queryset = crustdb_main.objects.order_by(columnKey)
+                self.queryset = self.queryset.order_by(columnKey)
+                # self.queryset = crustdb_main.objects.order_by(columnKey)
             else: # 'descend
-                self.queryset = crustdb_main.objects.order_by('-'+columnKey)
+                self.queryset = self.queryset.order_by('-'+columnKey)
+                # self.queryset = crustdb_main.objects.order_by('-'+columnKey)
+
         paginator = self.pagination_class()
         result_page = paginator.paginate_queryset(self.queryset, request)
         serializer = crustdbSerializer(result_page, many=True)
