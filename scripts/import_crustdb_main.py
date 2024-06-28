930a11b477395e43296c10ed4940827a5737737f
Yecats77 <liusitong327@gmail.com>
2024-06-28 18:52:31 +0800

    [2024/6/28] Small Update

--- scripts/import_crustdb_main.py
+++ scripts/import_crustdb_main.py
@@ -41,12 +41,19 @@ def add_data():
     with open(local_settings.CRUSTDB_DATABASE + 'main/CRUST_OUTPUT_INFO.csv', 'r') as f:
         csv_lines = f.readlines()
 
+    
+    count_repeat = 0
+    count_sample = 0
+    count_failed = 0
+
+    print('csv_lines', len(csv_lines))
     for line in csv_lines[1:]:
         # l = line.strip().split(",")
         l = list(csv.reader([line], delimiter=',', quotechar='"'))[0]
         data_uid = l[1]
 
         if data_uid in convergence_fail_record:
+            count_failed += 1
             continue
 
         uniq_data_uid = data_uid.strip()[:-5]
@@ -55,12 +62,14 @@ def add_data():
         crustdb_main_qs = crustdb_main.objects.filter(
             uniq_data_uid=uniq_data_uid)
         if len(crustdb_main_qs) > 0:
+            count_repeat += 1
+            crustdb_main_obj = crustdb_main_qs.first()
             # this record already exists, just add repeat_data_uid to the list
-            repeat_data_uid_list = crustdb_main_qs.first().repeat_data_uid_list
+            repeat_data_uid_list = crustdb_main_obj.repeat_data_uid_list
             repeat_data_uid_list.append(repeat_data_uid)
-            crustdb_main_qs.update(repeat_data_uid_list=repeat_data_uid_list)
-            crustdb_main_qs.update(
-                conformation_num=crustdb_main_qs.first().conformation_num + 1)
+            crustdb_main_obj.repeat_data_uid_list = repeat_data_uid_list
+            crustdb_main_obj.conformation_num = crustdb_main_obj.conformation_num + 1
+            crustdb_main_obj.save()
             continue
 
         species = l[5]
@@ -86,9 +95,10 @@ def add_data():
         #     print('========= Error when adding data to DB ==========')
         #     print('Species', species)
         #     break
-
-        log_lines = None
         try:
+            count_sample += 1
+
+            log_lines = None
             if species_common == 'merfish_ileum':
                 _data_uid = data_uid.replace('MERFISH_MICE_ILEUM.csv', 'transcripts.gem.csv')
             elif species_common == 'Xenium_BreastCancer':
@@ -129,6 +139,10 @@ def add_data():
             print('UID:', data_uid)
             print()
             # print("log_lines:", log_lines, '\n')
+        
+    print('count_repeat', count_repeat)
+    print('count_sample', count_sample)
+    print('count_failed', count_failed)
 
 
 if __name__ == "__main__":
